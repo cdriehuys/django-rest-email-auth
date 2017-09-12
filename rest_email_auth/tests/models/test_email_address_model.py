5af4d496157d338779be7b6c5261efd575c0ceb1
Chathan Driehuys <cdriehuys@gmail.com>
2017-09-12 15:43:13 -0400

    Add method to send verification email.

--- rest_email_auth/tests/models/test_email_address_model.py
+++ rest_email_auth/tests/models/test_email_address_model.py
@@ -1,3 +1,8 @@
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 from rest_email_auth import models
 
 
@@ -10,6 +15,22 @@ def test_create(user_factory):
         user=user_factory())
 
     assert not email.is_verified
+
+
+def test_send_confirmation(email_factory):
+    """
+    Sending a confirmation should create an ``EmailConfirmation``
+    instance and send a verification email.
+    """
+    email = email_factory()
+
+    with mock.patch(
+            'rest_email_auth.models.EmailConfirmation.send',
+            autospec=True) as mock_send:
+        email.send_confirmation()
+
+    assert email.confirmations.count() == 1
+    assert mock_send.call_count == 1
 
 
 def test_string_conversion(email_factory):
