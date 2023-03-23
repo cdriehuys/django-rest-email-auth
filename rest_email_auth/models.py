d0f6f0e7b2c9c8b11603ff6932ff7368d6d283ea
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2023-03-23 15:43:12 +0100

    removed deprecated function

--- rest_email_auth/models.py
+++ rest_email_auth/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.db import models, transaction
 from django.utils import timezone
 from django.utils.crypto import get_random_string
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 import email_utils
 
