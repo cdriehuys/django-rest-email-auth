3cb1c23a2f187d17387fe87661865d0d7bc21587
Federico Gibertoni <f.gibertoni@certego.net>
2025-09-12 16:28:39 +0200

    Fix: DRF singularity in tests

--- rest_email_auth/tests/serializers/test_email_serializer.py
+++ rest_email_auth/tests/serializers/test_email_serializer.py
@@ -95,7 +95,8 @@ def test_serialize(email_factory):
 
     expected = {
         "id": email.id,
-        "created_at": email.created_at.isoformat(),
+        # DRF truncates last bytes and adds a Z for UTC timestamps
+        "created_at": email.created_at.isoformat()[:-6] + 'Z',
         "email": email.email,
         "is_primary": email.is_primary,
         "is_verified": email.is_verified,
