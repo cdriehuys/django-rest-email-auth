c0fafed815600493dbdcd87a1f55ebb97a68e6c6
Federico Gibertoni <f.gibertoni@certego.net>
2025-09-12 16:12:12 +0200

    fix: pinned timezone in tests

--- test_settings.py
+++ test_settings.py
@@ -22,6 +22,19 @@ TEMPLATES = [
 
 ROOT_URLCONF = "test_urls"
 
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
 
 # Basic settings required for the app
 REST_EMAIL_AUTH = {
